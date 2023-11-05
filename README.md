3b4859aa64bf2f7ec7088374a5ef348388fb3a1a
Jandrei Timoteo <jandrei_tim@yahoo.com>
2023-11-05 15:48:06 -0800

    Added Purpose, Built-with, Usage, Website, Credits and Questiion in README.md and also added comments in server.js

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # 11.-Express.js-Challenge-Note-Taker
 
+# Purpose
+This note-taker application can be used to write and save notes. This application also uses Express.js back end and will save and retrieve note data from a JSON file.
+
+# Built With
+* JavaScript
+* HTML
+* CSS
+* Express.js
+
+# Usage
+Run npm init -y and npm i.
+
+# Website
+
+
+# Credits
+Created by Jandrei Timoteo with the help of Tutor Corrado Alfano.
+
+# Questions
+For additional questions and information, please go to https://github.com/Jdrei01 or reach out via email at jandreitim@gmail.com.
--- server.js
+++ server.js
@@ -1,29 +1,36 @@
+// import express, node, and file system
 const express = require('express');
 const app = express();
 const path = require('path');
 
+// middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// static public folder
 app.use(express.static(path.join(__dirname, 'public')))
 
+// index.html public folder
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname,'./public/index.html' ))
 })
 
+// notes.html public folder
 app.get('/notes', function (req, res) {
     res.sendFile(path.join(__dirname,'./public/notes.html' ))
 })
 
+// db.json folder 
 app.get('/api/notes', function (req, res) {
     res.sendFile(path.join(__dirname, '/db/db.json' ))
 })
 
+// 
 app.post('/api/notes', function(req, res) {
     console.log('req -> ', req);
 
 })
 
-
+// listen for connections on specfied ports
 app.listen(3001, () => console.log('App now running on http://127.0.0.1:3001'));
 
